36d0b632ec72921a9957d99099c3f734a5f8b42f
lizzb <huzzah.its.lizz@gmail.com>
2013-07-20 04:20:08 -0400

    css file is more readable with line breaks manually enetered

--- resources/sass/config.rb
+++ resources/sass/config.rb
@@ -1,18 +1,18 @@
 # Get the directory that this configuration file exists in
-dir = File.dirname(__FILE__)
+##dir = File.dirname(__FILE__)
 
 # Load the sencha-touch framework automatically.
-load File.join(dir, '..', '..', '..', '..', 'resources', 'themes')
+##load File.join(dir, '..', '..', '..', '..', 'resources', 'themes')
 
 # Compass configurations
-sass_path = dir
-css_path = File.join(dir, "..", "css")
+##sass_path = dir
+##css_path = File.join(dir, "..", "css")
 
 # Require any additional compass plugins here.
-fonts_path = File.join(dir, '../../../../resources/themes/fonts/')
-images_dir = File.join(dir, "..", "images")
-output_style = :expanded #was compressed
-environment = :production
+##fonts_path = File.join(dir, '../../../../resources/themes/fonts/')
+##images_dir = File.join(dir, "..", "images")
+##output_style = :expanded #was compressed
+##environment = :production
 
 
 # http://www.netmagazine.com/tutorials/code-smarter-css-sass
